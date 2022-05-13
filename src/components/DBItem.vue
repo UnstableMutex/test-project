052ce6409a55f5ca3a1baae855e8527eef7329bb
Dmitry <stormbringer-s@mail.ru>
2022-05-13 22:23:22 +0300

    changes

---
+++ src/components/DBItem.vue
@@ -0,0 +1,24 @@
+<template>
+    
+<li>
+
+
+   <div>  {{ name }}</div> <button>Edit</button>
+
+
+
+
+</li>
+</template>
+
+<script>
+export default {
+  name: "DBItem",
+    props: {
+    id: Number,
+    name: String
+  },
+
+
+}
+</script>
